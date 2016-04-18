548f4ccf49c334536fa22837bd51cb9566ced489
owlab-io <contact@owlab.io>
2016-04-18 11:06:20 +0200

    added some code to session 1

--- js/session1/js/app.js
+++ js/session1/js/app.js
@@ -1,29 +1,64 @@
-function init() {
+/*global window, document*/
+
+function log(data) {
+    "use strict";
+    window.console.log(data);
+}
+
+function select(sCSS) {
+    "use strict";
+    return document.querySelector(sCSS);
+}
+
+var init = (function init() {
     "use strict";
 
-    var a,
-        exo1,
+    var exo1,
         exo2,
         exo3,
         exo4,
         exo5,
-        log,
         observe;
 
-    window.onclick = observe;
+    window.addEventListener('load', function () {
+        select('body').addEventListener('click', observe);
+    });
 
-    log = function (data) {
-        window.console.log(data);
+
+    exo1 = function () {
+        log('exo 1');
+    };
+
+    exo2 = function () {
+        log('exo 2');
+    };
+
+    exo3 = function () {
+        log('exo 3');
+    };
+
+    exo4 = function () {
+        log('exo 4');
+    };
+
+    exo5 = function () {
+        log('exo 5');
     };
 
     observe = function (evt) {
-
         var source = evt.target || evt.srcElement;
 
-        if (source.id === 'exo_1')
+        if (source.id === 'exo_1') {
             exo1();
-
+        } else if (source.id === 'exo_2') {
+            exo2();
+        } else if (source.id === 'exo_3') {
+            exo3();
+        } else if (source.id === 'exo_4') {
+            exo4();
+        } else {
+            exo5();
+        }
     };
 
-
-}
+}()); // @end init()
