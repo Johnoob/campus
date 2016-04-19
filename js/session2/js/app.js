49769c7c4ca95a59bb76e5ff0290f4bc6db4f5a1
owlab-io <contact@owlab.io>
2016-04-19 10:38:13 +0200

    added response for exos s2e1-2-3-4

--- js/session2/js/app.js
+++ js/session2/js/app.js
@@ -15,7 +15,7 @@ function selectAll(sCSS) {
     return document.querySelectorAll(sCSS);
 }
 
-var init = (function init() {
+var session2 = (function init() {
     "use strict";
 
     var exo1,
@@ -23,7 +23,9 @@ var init = (function init() {
         exo3,
         exo4,
         exo5,
-        observe;
+        inverserCouleurs,
+        observe,
+        swapLetters;
 
     /*
         point de départ du programme =>
@@ -36,14 +38,99 @@ var init = (function init() {
     });
 
     exo1 = function () {
+        window.alert("Clicker sur un des carrés ci-dessous pour inverser les couleurs");
+    };
 
+    inverserCouleurs = function (carre) {
+        var autreCarre, couleur;
+        /*
+            on utilise la valeur de this passée en paramètre de la fonction dans le HTML.
+            dans ce cas de figure, this représente l'élément cliqué...
+        */
+        couleur = carre.style.background;
+        autreCarre = carre.nextElementSibling;
+
+        if (autreCarre.className !== 'block') {
+            /*
+                Si l'élément frère droit du carré cliqué n'a pas la classe css block,
+                autreCarre est le block à gauche du carré cliqué.
+            */
+            autreCarre = carre.previousElementSibling;
+        }
+
+        carre.style.background = autreCarre.style.background;
+        autreCarre.style.background = couleur;
     };
 
     exo2 = function () {
+        var data, input;
+        input = document.querySelector("#chaine_a_inverser");
+        data = swapLetters(input.value);
+        log("chaîne inversée => ");
+        log(data);
+        input.value = data;
+    };
 
+    swapLetters = function (chaine) {
+        var tableau, i, resultat;
+
+        tableau = chaine.split('');
+        resultat = '';
+
+        for (i = tableau.length - 1; i >= 0; i -= 1) {
+            resultat += tableau[i];
+        }
+
+        return resultat;
     };
 
     exo3 = function () {
+        var tmp = "";
+        log('+++++ exo 3 -> DRRRAW +++++');
+
+        function drawColumn(col) {
+            var i, str;
+            str = '| ';
+            for (i = 0; i < col; i += 1) {
+                str += i  + ' | ';
+            }
+            str += "\n";
+            return str;
+        }
+
+        function drawRow(row, col) {
+            var i, str = '';
+            for (i = 0; i < row; i += 1) {
+                str += drawColumn(col);
+            }
+            log(str);
+            str += "\n";
+            return str;
+        }
+
+
+        tmp += drawRow(1, 1);
+        tmp += drawRow(1, 2);
+        tmp += drawRow(1, 3);
+        tmp += drawRow(1, 4);
+        tmp += drawRow(1, 5);
+
+
+        document.querySelector("#result_draw").innerHTML = tmp;
+    };
+
+    exo4 = function () {
+        var test, chaine, afficher;
+        afficher = document.querySelector("#resultat_palindrome");
+        chaine = document.querySelector("#saisie_palindrome").value;
+        test = chaine === swapLetters(chaine);
+        log("est palindrome ? " + test);
+
+        if (test) {
+            afficher.innerHTML = chaine + " est palindrome";
+        } else {
+            afficher.innerHTML = chaine + " n'est pas palindrome";
+        }
 
     };
 
@@ -57,16 +144,24 @@ var init = (function init() {
         if (source.id === 'exo_1') {
             /* si le bouton clické possède l'identifiant exo_1, on exécute la fonction exo1() */
             exo1();
-        } else if (source.id === 'exo_2') {
+
+        } else if (source.id === 'exo_2' || source.id === 'inverser_chaine') {
             /* ... et ainsi de suite ... */
             exo2();
+
         } else if (source.id === 'exo_3') {
             exo3();
-        } else if (source.id === 'exo_4') {
+
+        } else if (source.id === 'exo_4' || source.id === "verifier_palindrome") {
             exo4();
-        } else {
+
+        } else if (source.id === 'exo_5') {
             exo5();
         }
+    };
+
+    return {
+        inverserCouleurs: inverserCouleurs
     };
 
 }()); // @end init()
