d32887f24bcdf6a17f33258183a81698497e2524
owlab-io <contact@owlab.io>
2016-04-29 13:39:38 +0200

    added code to sesseion 3 exo1 and exo3 and assets -> images

--- js/session3/js/app.js
+++ js/session3/js/app.js
@@ -15,205 +15,107 @@ function selectAll(sCSS) {
     return document.querySelectorAll(sCSS);
 }
 
-var session2 = (function init() {
+/*
+la fonction session3 est une IIFE (fonction auto-exécutée)
+*/
+var session3 = (function init() {
     "use strict";
 
     var exo1,
-        exo2,
-        exo3,
-        exo4,
-        exo5,
-        inverserCouleurs,
-        observe,
-        swapLetters,
-        User;
+    exo2,
+    exo3,
+    observer;
 
     /*
-        point de départ du programme =>
-        on demande à window (l'objet global JS) d'attendre le chargement complet
-        du document HTML. Une fois fait, on écoute les clicks sur tout le body.
-        A chaque click, la fonction observe est exécutée.
+    point de départ du programme =>
+    on demande à window (l'objet global JS) d'attendre le chargement complet
+    du document HTML.
+    Une fois fait, on écoute les change sur le select (la fonction exo1 est alors exécutée)
     */
+
     window.addEventListener('load', function () {
-        select('body').addEventListener('click', observe);
+        observer();
     });
 
-    exo1 = function () {
-        window.alert("Clicker sur un des carrés ci-dessous pour inverser les couleurs");
-    };
-
-    inverserCouleurs = function (carre) {
-        var autreCarre, couleur;
-        /*
-            on utilise la valeur de this passée en paramètre de la fonction dans le HTML.
-            dans ce cas de figure, this représente l'élément cliqué...
-        */
-        couleur = carre.style.background;
-        autreCarre = carre.nextElementSibling;
-
-        if (autreCarre.className !== 'block') {
-            /*
-                Si l'élément frère droit du carré cliqué n'a pas la classe css block,
-                autreCarre est le block à gauche du carré cliqué.
-            */
-            autreCarre = carre.previousElementSibling;
-        }
-
-        carre.style.background = autreCarre.style.background;
-        autreCarre.style.background = couleur;
-    };
-
-    exo2 = function () {
-        var data, input;
-        input = document.querySelector("#chaine_a_inverser");
-        data = swapLetters(input.value);
-        log("chaîne inversée => ");
-        log(data);
-        input.value = data;
-    };
-
-    swapLetters = function (chaine) {
-        var tableau, i, resultat;
-
-        if (!chaine.length) {
-            window.alert('attention -> chaîne vide');
-        }
-        tableau = chaine.split('');
-        resultat = '';
-
-        for (i = tableau.length - 1; i >= 0; i -= 1) {
-            resultat += tableau[i];
-        }
-
-        return resultat;
-    };
-
-    exo3 = function () {
-        var tmp = "";
-        log('+++++ exo 3 -> DRRRAW +++++');
-
-        function drawColumn(col) {
-            var i, str;
-            str = '| ';
-            for (i = 0; i < col; i += 1) {
-                str += i  + ' | ';
-            }
-            str += "\n";
-            return str;
-        }
-
-        function drawRow(row, col) {
-            var i, str = '';
-            for (i = 0; i < row; i += 1) {
-                str += drawColumn(col);
-            }
-            log(str);
-            str += "\n";
-            return str;
-        }
-
-
-        tmp += drawRow(1, 1);
-        tmp += drawRow(1, 2);
-        tmp += drawRow(1, 3);
-        tmp += drawRow(1, 4);
-        tmp += drawRow(1, 5);
-
-
-        document.querySelector("#result_draw").innerHTML = tmp;
-    };
-
-    exo4 = function () {
-        var test, chaine, afficher;
-        afficher = document.querySelector("#resultat_palindrome");
-        chaine = document.querySelector("#saisie_palindrome").value;
-        test = chaine === swapLetters(chaine);
-        log("est palindrome ? " + test);
-
-        if (test) {
-            afficher.innerHTML = chaine + " est palindrome";
-        } else {
-            afficher.innerHTML = chaine + " n'est pas palindrome";
-        }
-
-    };
-
-    exo5 = function () {
-        var i, count, users, name, age, resultat, message, btnBonjour;
-        /* on récupère le nombre d'utilisateurs à créer de depuis l'input HTML number*/
-        count = Number(document.querySelector("#nombre_users").value);
-        btnBonjour = document.querySelector("#bonjour_users");
-        /* on créé un tableau vide pour y stocker ensuite les users */
-        users = [];
-
-        resultat = document.querySelector("#resultat_users");
-
-        /* on utilise la variable count pour créer le nombre d'users souhaités */
-        for (i = 0; i < count; i += 1) {
-
-            do {
-                name = window.prompt("Saisir le nom de l'user n°" + (i + 1));
-
-            } while (!name || isFinite(name));
-
-            do {
-                age = window.prompt("Saisir l'age de l'user n°" + (i + 1));
-
-            } while (!age || !isFinite(age));
-
-            users.push(new User(name, age));
-        }
-
-        /* on affiche un message différent si on a créé 1 ou plusieurs users */
-        message = (users.length === 1) ? "user a été créé" : "users ont été créés";
-        /* on affiche le nombre d'users créés dans un block HTML*/
-        resultat.innerHTML = users.length + ' ' + message;
-        log(users);
-
-        btnBonjour.onclick = function helloUser() {
-            var j;
-            if (users.length <= 1) {
-                window.alert("Il n'y a personne à qui dire bonjour");
-            }
-            for (j = 1; j < users.length; j += 1) {
-                log("L'user nommé " + users[j].name + " dit : \n");
-                users[j].direBonjour(users[j - 1]);
-            }
-        };
-    };
-
-    User = function (name, age) {
-        this.name = name;
-        this.age = age;
-        this.direBonjour = function (qui) {
-            log("Bonjour " + qui.name);
-        };
-    };
-
-    observe = function (evt) {
+    exo1 = function (evt) {
         var source = evt.target || evt.srcElement;
 
-        if (source.id === 'exo_1') {
-            /* si le bouton clické possède l'identifiant exo_1, on exécute la fonction exo1() */
-            exo1();
+        function manageCSS(index) {
+            var square, previous;
+            square = selectAll('.squares .square')[index - 1];
+            previous = select('.squares .square.is-active');
 
-        } else if (source.id === 'exo_2' || source.id === 'inverser_chaine') {
-            /* ... et ainsi de suite ... */
-            exo2();
+            if (previous) {
+                previous.classList.remove('is-active');
+            }
+            if (square) {
+                square.classList.add('is-active');
+            }
+        }
 
-        } else if (source.id === 'exo_3') {
-            exo3();
+        manageCSS(source.selectedIndex);
+    };
 
-        } else if (source.id === 'exo_4' || source.id === "verifier_palindrome") {
-            exo4();
 
-        } else if (source.id === 'exo_5' || source.id === "creer_users") {
-            exo5();
+    exo2 = function () {
+        log('iiii')
+    };
+
+
+    exo3 = function (evt) {
+        var source, trackerStatus, setTracker;
+
+        source = evt.target || evt.srcElement;
+        trackerStatus = source.id === 'activer_tracker';
+
+        setTracker = function (status) {
+            log(status);
+            var animationFrame, tracker, x, y, move, getMousePosition;
+
+            tracker = select('.mouse-tracker');
+            animationFrame = window.requestAnimationFrame = window.requestAnimationFrame ||
+            window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
+            window.msRequestAnimationFrame;
+
+            getMousePosition = function (evt) {
+                x = evt.x;
+                y = evt.y;
+            };
+
+            move = function () {
+                tracker.style.top = y + 'px';
+                tracker.style.left = x + 'px';
+                tracker.querySelector('#y_pos').textContent = (!y) ? '' : 'y: ' + y + 'px';
+                tracker.querySelector('#x_pos').textContent = (!x) ? '' : 'x: ' + x + 'px';
+                animationFrame(move);
+            };
+
+            if (status) {
+                tracker.classList.add('is-active');
+                window.addEventListener('mousemove', getMousePosition);
+                animationFrame(move);
+            } else {
+                tracker.classList.remove('is-active');
+                window.removeEventListener('mousemove', getMousePosition);
+            }
+        };
+
+        setTracker(trackerStatus);
+    };
+
+    observer = function () {
+        var i, trackerTriggers;
+
+        trackerTriggers = selectAll('.tracker-trigger');
+
+        select('.selector').addEventListener('change', exo1);
+
+        select('.scroll-top .img').addEventListener('click', exo2);
+
+        for (i = 0; i < trackerTriggers.length; i += 1) {
+            trackerTriggers[i].onchange = exo3;
         }
     };
 
-    return {
-        inverserCouleurs: inverserCouleurs
-    };
 
 }()); // @end init()
